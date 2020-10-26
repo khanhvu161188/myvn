dcd6a3a6fd771a9e27138bef5a65b3bed838c08e
trieu.vo <trieugia008@gmail.com>
2020-10-26 10:49:51 +0700

    remove inmemory_httpClient

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -24,12 +24,6 @@ import {AuthService} from './auth/services/authService';
     AppRoutingModule,
     HttpClientModule,
 
-    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
-    // and returns simulated server responses.
-    // Remove it when a real server is ready to receive requests.
-    HttpClientInMemoryWebApiModule.forRoot(
-      InMemoryDataService, { dataEncapsulation: false }
-    ),
 
     BrowserAnimationsModule,
 
